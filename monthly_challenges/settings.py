90611462a1b4ffb3310c2018a2ba3c9113a979bf
AnthonerMonzon <puppz03@gmail.com>
2024-06-11 01:46:08 +0800

    recommit deployment

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)a@36-9nin+7v1*!%*b41dmd4%1&8z!2zi)eu&dd8$!wy1nl8n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://acm-monthly-challenges.onrender.com']
 
 
 # Application definition
