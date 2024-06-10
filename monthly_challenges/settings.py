e37c8cd33e2d7231898b5300fc8174b4724579c2
AnthonerMonzon <puppz03@gmail.com>
2024-06-10 23:54:20 +0800

    static files

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'challenges', # officiall add this apps
+    'challenges', # officially add this apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
