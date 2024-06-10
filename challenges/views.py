c27b94cc5928b78e1e04c936e0708dcee027ba18
AnthonerMonzon <puppz03@gmail.com>
2024-06-10 22:49:56 +0800

    template samples

--- challenges/views.py
+++ challenges/views.py
@@ -17,24 +17,20 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!"
+    "december": None
 }
 
 # Create your views here.
 
 # redirecting number month to string month
 
+
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</li>"
-        
     
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -42,14 +38,18 @@ def monthly_challenge_by_number(request, month):
     if month > len(months):
         return HttpResponseNotFound("<h1>Invalid month</h1>")
     redirect_month = months[month - 1]
-    redirect_path = reverse("month-challenge", args=[redirect_month]) #/challenge/january
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/january
     return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"  # string interpolation
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
+        # DTL - Django Text Language
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
